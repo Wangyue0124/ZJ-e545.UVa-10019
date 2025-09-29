8fca94a388660ba4420d4bec4e0f467f662e7fc6
Wangyue0124 <diego960124@gmail.com>
2025-09-29 22:17:31 +0800

    Add files via upload

---
+++ e545. 10019 - Funny Encryption Method.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int dec(int val);
+int hex(int val);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    while (n--) {
+        int val;
+        cin >> val;
+        cout << dec(val) << " " << hex(val) << "\n";
+    }
+    return 0;
+}
+int dec(int val) {
+    int tal = 0;
+    while (val) {
+        if (val % 2 == 1) tal++;
+        val /= 2;
+    }
+    return tal;
+}
+int hex(int val) {
+    int tal = 0;
+    while (val) {
+        int num = val % 10;
+        tal += dec(num);
+        val /= 10;
+    }
+    return tal;
+}
